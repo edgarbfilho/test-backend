3c0cea7c604e2932335effb0aaa7739aa3be5d42
Edgar Barbosa Filho <edgarbarbosafilho@Mac-mini-de-Edgar.local>
2026-03-19 11:12:01 -0300

    estrutura inicial organizada

---
+++ api/index.ts
@@ -0,0 +1,32 @@
+import 'reflect-metadata'
+import { VercelRequest, VercelResponse } from '@vercel/node'
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from '../src/app.module'
+import { ExpressAdapter } from '@nestjs/platform-express'
+import express from 'express'
+
+const server = express()
+let appPromise: Promise<any>
+
+async function bootstrap() {
+  if (!appPromise) {
+    appPromise = (async () => {
+      const app = await NestFactory.create(AppModule, new ExpressAdapter(server))
+
+      app.enableCors({
+        origin: '*',
+        credentials: true,
+      })
+
+      await app.init()
+      return server
+    })()
+  }
+
+  return appPromise
+}
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  const app = await bootstrap()
+  return app(req, res)
+}
